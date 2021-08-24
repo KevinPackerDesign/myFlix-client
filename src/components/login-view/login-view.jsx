b337535f92d6aabe036800492493533297c652b5
Kevinpacker <kevinpackerdesign@gmail.com>
2021-08-23 19:21:34 -0700

    clearned merge

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,25 +1,10 @@
 import React, { useState } from "react";
-<<<<<<< HEAD
-=======
 import PropTypes from "prop-types";
->>>>>>> main
 
 export function LoginView(props) {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
-<<<<<<< HEAD
-  const handleSubmit = () => {
-    e.preventDefault();
-    console.log(username, password);
-  };
-
-  // props.onLoggedIn(username);
-
-  return (
-    <form>
-      <lable>
-=======
   const handleSubmit = (e) => {
     e.preventDefault();
     props.onLoggedIn(username);
@@ -28,41 +13,28 @@ export function LoginView(props) {
   return (
     <form>
       <label>
->>>>>>> main
         Username:
         <input
           type="text"
           value={username}
           onChange={(e) => setUsername(e.target.value)}
         />
-<<<<<<< HEAD
-      </lable>
-      <lable>
-=======
       </label>
       <label>
->>>>>>> main
         Password:
         <input
           type="password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
-<<<<<<< HEAD
-      </lable>
-=======
       </label>
->>>>>>> main
       <button type="button" onClick={handleSubmit}>
         Submit
       </button>
     </form>
   );
 }
-<<<<<<< HEAD
-=======
 
 LoginView.propTypes = {
   onLoggedIn: PropTypes.func.isRequired,
 };
->>>>>>> main
--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -26,23 +26,13 @@ export function RegistrationView(props) {
     e.preventDefault();
     axios
       .post("https://kpmyflix.herokuapp.com/users", {
-<<<<<<< HEAD
-        FirstName: firstname,
-        LastName: lastname,
-=======
->>>>>>> main
         Username: username,
         Password: password,
         Email: email,
         Birthday: birthday,
       })
-<<<<<<< HEAD
-      .the((response) => {
-        const data = respose.data;
-=======
       .then((response) => {
         const data = response.data;
->>>>>>> main
         console.log(data);
         alert("registration successfull! Please Login.");
       })
@@ -50,73 +40,26 @@ export function RegistrationView(props) {
         alert("This user is already registered.");
         console.log("error registering the user");
       });
-<<<<<<< HEAD
-=======
     onRegistration(username);
->>>>>>> main
   };
 
   return (
     <form>
-<<<<<<< HEAD
-      <lable>
-        First Name:
-        <input
-          type="text"
-          value={name}
-          onChange={(e) => setFirstName(e.target.value)}
-        />
-      </lable>
-      <lable>
-        Last Name:
-        <input
-          type="text"
-          value={name}
-          onChange={(e) => setLastName(e.target.value)}
-        />
-      </lable>
-      <lable>
-=======
       <label>
->>>>>>> main
         Username:
         <input
           type="text"
           value={username}
           onChange={(e) => setUsername(e.target.value)}
         />
-<<<<<<< HEAD
-      </lable>
-      <lable>
-=======
       </label>
       <label>
->>>>>>> main
         Password:
         <input
           type="password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
-<<<<<<< HEAD
-      </lable>
-      <lable>
-        Email:
-        <input
-          type="text"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-      </lable>
-      <lable>
-        Birthday:
-        <input
-          type="text"
-          value={birth}
-          onChange={(e) => setBirthday(e.target.value)}
-        />
-      </lable>
-=======
       </label>
       <label>
         Email:
@@ -134,7 +77,6 @@ export function RegistrationView(props) {
           onChange={(e) => setBirthday(e.target.value)}
         />
       </label>
->>>>>>> main
       <button type="button" onClick={handleSubmit}>
         Submit
       </button>
